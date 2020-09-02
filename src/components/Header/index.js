f5dff8074fa99aacf16d72460de374e42a6b09ca
Rodrigo <rpessolato@gmail.com>
2020-09-02 12:28:02 -0300

    Listing schools from json file

---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './style.css';
+
+const Header = () => {
+  return (
+    <header id="main-header">
+      <div>
+        <h3>SME Piracicaba</h3>
+      </div>
+      <div>
+        <input type="text" name="search" />
+      </div>
+    </header>
+  )
+}
+
+export default Header;
+
